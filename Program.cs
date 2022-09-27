a345abdbc8ab7d77fa2eda0fa54b8a4f6224cf84
cemileca <97739544+cemileca@users.noreply.github.com>
2022-09-27 12:18:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,46 +4,59 @@
     {
         static void Main(string[] args)
         {
-            #region MüşteriEKle
 
-
-            Musteri musteri = new Musteri();
-            musteri.MusteriID = 1;
-            musteri.MusteriAdi = "Yeliz";
-            musteri.MusteriSoyadi = "Pınar";
-
-            Musteri musteri1 = new Musteri();
-            musteri.MusteriID = 2;
-            musteri.MusteriAdi = "Ahmet";
-            musteri.MusteriSoyadi = "Demir";
-
-            Musteri musteri2 = new Musteri();
-            musteri.MusteriID = 3;
-            musteri.MusteriAdi = "Ferid";
-            musteri.MusteriSoyadi = "Zengin";
-
-            Musteri musteri3 = new Musteri();
-            musteri.MusteriID = 4;
-            musteri.MusteriAdi = "Niyazi";
-            musteri.MusteriSoyadi = "Konuk";
-
-            Musteri[] musteriDizisi = { musteri, musteri1,musteri2,musteri3  };
-
-
-            #endregion
-
-                     Console.WriteLine(@"Yapmak istediğiniz işlemi Seçiniz:
+            Console.WriteLine(@"Yapmak istediğiniz işlemi Seçiniz:
 Müşteri Listele:  1
 Müşteri Ekle:     2
 Müşteri Sil:      3");
             int GirilenDeger = Convert.ToInt32(Console.ReadLine());
+
+            #region MüşteriEKle
+
+
+            Musteri[] musteriDizisi = new Musteri[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Musteri musteri = new Musteri();
+                switch (i)
+                {
+                    case 0:
+                        musteri.MusteriID = 1;
+                        musteri.MusteriAdi = "Yeliz";
+                        musteri.MusteriSoyadi = "Pınar";
+                        break;
+                    case 1:
+                        musteri.MusteriID = 2;
+                        musteri.MusteriAdi = "Ahmet";
+                        musteri.MusteriSoyadi = "Demir";
+                        break;
+                    case 2:
+                        musteri.MusteriID = 3;
+                        musteri.MusteriAdi = "Ferid";
+                        musteri.MusteriSoyadi = "Zengin";
+                        break;
+                    case 3:
+                        musteri.MusteriID = 4;
+                        musteri.MusteriAdi = "Niyazi";
+                        musteri.MusteriSoyadi = "Konuk";
+                        break;
+                }
+                musteriDizisi[i] = musteri;
+            }
+
+            //  Musteri[] musteriDizisi = { musteri, musteri1, musteri2, musteri3 };
+
+
+            #endregion
+
             MusteriManager musteriYonet = new MusteriManager();
 
             switch (GirilenDeger)
             {
                 case 1:
                     Console.Clear();
-                    
+
                     musteriYonet.MusteriListele(musteriDizisi);
 
                     Console.WriteLine(@"
@@ -53,7 +66,18 @@ Müşteri Ekle:     2
 Müşteri Sil:      3
 Ana menüye dön:   4
 ");
+                    int degerGir1 = Convert.ToInt32(Console.ReadLine());
+                    if (degerGir1 == 1)
+                        goto case 1;
+                    else if (degerGir1 == 2)
+                        goto case 2;
+                    else if (degerGir1 == 3)
+                        goto case 3;
+                    else
+                         Console.WriteLine("Sistemden çıkış yapıldı ve Kapatıldı");
+                   
                     break;
+                   
                 case 2:
 
                     musteriYonet.MusteriEkle(005, "Hayriye", "Solmaz");
@@ -66,6 +90,16 @@ Müşteri Ekle:     2
 Müşteri Sil:      3
 Ana menüye dön:   4
 ");
+                    int degerGir2 = Convert.ToInt32(Console.ReadLine());
+                    if (degerGir2 == 1)
+                        goto case 1;
+                    else if (degerGir2 == 2)
+                        goto case 2;
+                    else if (degerGir2 == 3)
+                        goto case 3;
+                    else
+                        Console.WriteLine("Sistemden çıkış yapıldı ve Kapatıldı");
+                   
                     break;
                 case 3:
                     Console.Clear();
@@ -78,6 +112,19 @@ Müşteri Ekle:     2
 Müşteri Sil:      3
 Ana menüye dön:   4
 ");
+                    int degerGir = Convert.ToInt32(Console.ReadLine());
+                    if (degerGir == 1)
+                        goto case 1;
+                    else if (degerGir == 2)
+                        goto case 2;
+                    else if (degerGir == 3)
+                        goto case 3;
+                    else
+                        Console.WriteLine("Sistemden çıkış yapıldı ve Kapatıldı");
+                    break;
+
+                default:
+                    Console.WriteLine("Sistemden çıkış yapıldı ve Kapatıldı");
                     break;
 
             }
